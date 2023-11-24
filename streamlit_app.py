286865d2c3a253c92da9d65589e72f10412a5f00
bewinsto7604 <wbenji7604@gmail.com>
2023-11-24 00:58:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ mydatabase = "BASELINE_STMT_STATISTICS"
 mysql_uri = f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{mydatabase}"
 db = SQLDatabase.from_uri(mysql_uri)
 agent_executor = create_sql_agent(
-    llm=OpenAI(temperature=0, openai_api_key="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"),
-    toolkit=SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0, openai_api_key="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")),
+    llm=OpenAI(temperature=0, openai_api_key="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"),
+    toolkit=SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0, openai_api_key="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")),
     verbose=True,
     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
 )
@@ -78,7 +78,7 @@ whatif_docs = text_splitter.split_documents(whatif_doc)
 num_total_characters = sum([len(x.page_content) for x in docs])
 print (f"Now you have {len(docs)} documents that have an average of {num_total_characters / len(docs):,.0f} characters (smaller pieces)")
 
-embeddings = OpenAIEmbeddings(openai_api_key="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")
+embeddings = OpenAIEmbeddings(openai_api_key="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")
 # Embed your documents and combine with the raw text in a pseudo db. Note: This will make an API call to OpenAI
 docsearch = FAISS.from_documents(docs, embeddings)
 # App title
