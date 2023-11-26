6e35c4754bd044459eb27fb8b8b83b919a9af7ec
bewinsto7604 <wbenji7604@gmail.com>
2023-11-26 08:49:12 -0500

    Update embed-chat.py

--- embed-chat.py
+++ embed-chat.py
@@ -6,15 +6,16 @@ from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 loader = TextLoader("padb-domain.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+doc = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=400)
+docs = text_splitter.split_documents(doc)
 
 embeddings = OpenAIEmbeddings()
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-db = FAISS.from_documents(docs, embeddings)
+docsearch = FAISS.from_documents(docs, embeddings)
 
 query = "What is QTXACHG?"
 docs = db.similarity_search(query)
