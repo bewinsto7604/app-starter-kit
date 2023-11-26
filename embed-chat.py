033b4bc6504828fe17f65826e13c35071e9f2200
bewinsto7604 <wbenji7604@gmail.com>
2023-11-26 08:39:59 -0500

    Update embed-chat.py

--- embed-chat.py
+++ embed-chat.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import getpass
 import os
+import time
 from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
