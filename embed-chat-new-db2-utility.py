fdde557f8506f17082394c13c7934644bd180020
bewinsto7604 <wbenji7604@gmail.com>
2023-12-10 23:44:34 -0500

    Update embed-chat-new-db2-utility.py

--- embed-chat-new-db2-utility.py
+++ embed-chat-new-db2-utility.py
@@ -27,18 +27,6 @@ memory = ConversationBufferMemory(memory_key="history", chat_memory=msgs)
 # template / prompt
 query1 = "When cond_code which is not equal to 0 it denotes a failed job. Show me jobname, utilname, error_reason_cd, error_reason for all failed jobs for event.utilname BMC AMI Reorg for Db2 in the last 2 days."
 query2 = "Show me all failed jobs for BMC AMI Reorg for Db2 in the last 3 days"
-template = """I want you to act as a customer service assistant for a
-BMC Utility for Db2. Respond in a friendly and helpful tone, with very concise answers.
-Make sure to ask the user relevant follow up questions.
-
-Given the context, these are the allowed cond_code
-When cond_code which is not equal to 0 it denotes a failed job. Always include jobname, utilname, error_reason_cd, error_reason in the output
-{context}
-
-{chat_history}
-Human: {human_input}
-Chatbot:"""
-
 str_prompt = 'Given the mapping delimited by triple backticks ``` ' + mapping_string + ' ``` translate the text delimited by triple quotes in a valid Elasticsearch DSL query """ ' + query2 + ' """. Give me only the json code part of the answer. Compress the json output removing spaces.'
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -80,15 +68,15 @@ for msg in msgs.messages:
     st.chat_message(msg.type).write(msg.content)
 if prompt := st.chat_input():
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": str_prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
-        st.markdown(prompt)
+        st.markdown(str_prompt)
     with st.chat_message("assistant", avatar=company_logo):
         message_placeholder = st.empty()
         # Send user's question to our chain
         response = ""
-        if "Show me" in prompt:
+        if "Show me" in str_prompt:
             print("enter")
             response = get_completion(query1)
             print(response)
