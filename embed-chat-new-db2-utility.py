c4fad160fd363e2c4dc2baf96f49792bf2cdb8c0
bewinsto7604 <wbenji7604@gmail.com>
2023-12-11 09:21:14 -0500

    Update embed-chat-new-db2-utility.py

--- embed-chat-new-db2-utility.py
+++ embed-chat-new-db2-utility.py
@@ -32,7 +32,8 @@ str_prompt = 'Given the mapping delimited by triple backticks ``` ' + mapping_st
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(model=model,messages=messages,temperature=0)
-    return response.choices[0].message["content"]
+    # return response.choices[0].message["content"]
+    return response
 
 # Initialization
 if 'generated' not in st.session_state:
