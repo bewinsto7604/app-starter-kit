ecb6ecaeba5ccc0d62d847c1afc88fa9fb8e2a84
bewinsto7604 <wbenji7604@gmail.com>
2023-12-10 23:01:48 -0500

    Update embed-chat-new-db2-utility.py

--- embed-chat-new-db2-utility.py
+++ embed-chat-new-db2-utility.py
@@ -16,6 +16,10 @@ import openai
 st.set_page_config(page_title="DB2 Utility assistant", layout="wide")
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+f = open('zcv1-171494-dba-mapping.json')
+mapping = json.load(f)
+mapping_string = json.dumps(mapping)
+
 # template / prompt
 query1 = "When cond_code which is not equal to 0 it denotes a failed job. Show me jobname, utilname, error_reason_cd, error_reason for all failed jobs for event.utilname BMC AMI Reorg for Db2 in the last 2 days."
 query2 = "Show me all failed jobs for BMC AMI Reorg for Db2 in the last 3 days"
@@ -33,10 +37,6 @@ Chatbot:"""
 
 prompt = 'Given the mapping delimited by triple backticks ``` ' + mapping_string + ' ``` translate the text delimited by triple quotes in a valid Elasticsearch DSL query """ ' + query2 + ' """. Give me only the json code part of the answer. Compress the json output removing spaces.'
 
-f = open('zcv1-171494-dba-mapping.json')
-mapping = json.load(f)
-mapping_string = json.dumps(mapping)
-
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(model=model,messages=messages,temperature=0)
@@ -45,4 +45,3 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 response = get_completion(prompt)
 
 print(response)
-
